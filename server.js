195b48877f36447f40264755bdecdc3b858f86b6
crossy87 <hannabmcross@gmail.com>
2018-01-29 22:56:50 +0000

    md files rendering with matching route

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const path = require('path');
+// const path = require('path');
+const docserver = require('docserver');
 
 const app = express();
 
@@ -10,13 +11,27 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(docserver({
+  dir: __dirname + '/docs',
+  url: '/docs/'}
+));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/template.html'));
-});
+// app.use(docserver({
+//   dir: __dirname + '/docs/jobs',
+//   url: '/jobs'}
+// ));
+//
+// app.use(docserver({
+//   dir: __dirname + '/docs/valves',
+//   url: '/valves'}
+// ));
+
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/template.html'));
+// });
 
 
 
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+  console.log(docserver.version + ` Listening on port: ${port}`);
 });

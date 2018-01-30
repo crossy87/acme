412d813d8df33e304b90c3cb232c0ce0ca8e9eee
crossy87 <hannabmcross@gmail.com>
2018-01-30 17:47:12 +0000

    tests returned as passing

--- test/content_test.js
+++ test/content_test.js
@@ -1,9 +1,11 @@
-/* globals api, expect, describe, it */
+/* globals api, expect, describe, it, req, res */
 
 // The application should be shipped with three tests:
 //
 // one that verifies that requests to valid URLs return a 200 HTTP status code
+
 // one that verifies that requests to valid URLS return a body that contains the HTML generated from the relevant index.md markdown file
+
 // one that verifies that requests to URLs that do not match content folders return a 404 HTTP status code
 
 require('./spec_helper');
@@ -26,6 +28,7 @@ describe('content test', () => {
         .expect(200, done);
     });
   });
+
   describe('GET /content/jobs', () => {
     it('should return a 200 response', done => {
       api
@@ -35,5 +38,51 @@ describe('content test', () => {
     });
   });
 
+});
 
+describe('md file type', () => {
+
+  it('should respond with correct content', done => {
+    api
+      .get('/content/about-page')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        res.text.should.match(/About/g);
+        done();
+      });
+  });
+
+  it('should respond with correct content', done => {
+    api
+      .get('/content/valves')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        res.text.should.match(/Valves/g);
+        done();
+      });
+  });
+
+  it('should respond with correct content', done => {
+    api
+      .get('/content/jobs')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        res.text.should.match(/Jobs/g);
+        done();
+      });
+  });
+
+
+});
+
+describe('error 404', () => {
+  it('wrong url', function(done) {
+    api
+      .get('/random')
+      .expect(404)
+      .end(function(err,res){
+        res.status.should.equal(404);
+        done();
+      });
+  });
 });
